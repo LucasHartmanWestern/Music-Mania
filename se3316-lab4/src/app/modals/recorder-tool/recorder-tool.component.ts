c7f0aebdea740fcf7de10a4c19b3ec1fd8beef11
Ethan <epigou@uwo.ca>
2022-11-29 12:12:45 -0500

    Add component for DMCA recording tools

---
+++ se3316-lab4/src/app/modals/recorder-tool/recorder-tool.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recorder-tool',
+  templateUrl: './recorder-tool.component.html',
+  styleUrls: ['./recorder-tool.component.scss']
+})
+export class RecorderToolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
